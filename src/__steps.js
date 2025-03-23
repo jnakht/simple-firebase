bd298fe93a41fb64f1a8096a82e013aaa432f6d2
jnakht <jisan.cse.cu@gmail.com>
2025-03-23 16:15:44 +0600

    event handler test

--- src/__steps.js
+++ src/__steps.js
@@ -5,5 +5,9 @@
  * 4. install firebase: npm install firebase
  * 5. add config file to your project
  * 6. DANGER: do not make public your firebase config file by pushing on public repository or something else
- * 
+ * 7. visit: go to Docs > Build > Authentication > Web > Get Started
+ * 8. export app from the firebase.config.js file: export default app
+ * 9. Login.jsx: import getAuth from firebase/auth
+ * 10. create const auth = getAuth(app)
+ * 11. import googleAuthProvider and create new authProvider
  *  */
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,15 @@
-
-
+import { getAuth } from "firebase/auth";
+import app from "../../firebase/firebase.init";
+import { GoogleAuthProvider } from "firebase/auth";
 const Login = () => {
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+    const handleGoogleSignIn = () => {
+        console.log('google is coming');
+    }
     return (
         <div>
-            <button className="btn">Google</button>
+            <button onClick={handleGoogleSignIn} className="btn">Google Login</button>
         </div>
     );
 };
