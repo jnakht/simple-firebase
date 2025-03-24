a88d4e5a6fbc5701517e5f00797964de2c65bfae
jnakht <jisan.cse.cu@gmail.com>
2025-03-25 00:32:46 +0600

    user data displayed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,17 @@
 import { getAuth, signInWithPopup } from "firebase/auth";
 import app from "../../firebase/firebase.init";
 import { GoogleAuthProvider } from "firebase/auth";
+import { useState } from "react";
 const Login = () => {
+    const [user, setUser] = useState(null);
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
     const handleGoogleSignIn = () => {
         signInWithPopup(auth, provider)
         .then(result => {
-            const user = result.user;
-            console.log(user)
+            const signedInUser = result.user;
+            console.log(signedInUser);
+            setUser(signedInUser);
         })
         .catch(error => {
             console.log("error: ", error.message)
@@ -17,6 +20,13 @@ const Login = () => {
     return (
         <div>
             <button onClick={handleGoogleSignIn} className="btn">Google Login</button>
+            {
+                user && <div>
+                <h3>User: {user.displayName}</h3>
+                <p>Email: {user.email}</p>
+                <img src={user.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
