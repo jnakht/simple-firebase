a1af8c93537b2f88cfd24f367e9362728047869c
jnakht <jisan.cse.cu@gmail.com>
2025-03-25 00:12:13 +0600

    google login recap successfull

--- src/components/LoginRecap/LoginRecap.jsx
+++ src/components/LoginRecap/LoginRecap.jsx
@@ -1,9 +1,24 @@
-
+import { getAuth, signInWithPopup } from "firebase/auth";
+import app from "../../firebase/firebaseRecap.init";
+import { GoogleAuthProvider } from "firebase/auth";
 
 const LoginRecap = () => {
+    const auth  = getAuth(app);
+    const provider = new GoogleAuthProvider();
+    
+    const handleGoogleLoginRecap = () => {
+        signInWithPopup(auth, provider)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => {
+            console.log('error is: ', error.message);
+        })
+    }
     return (
         <div>
-            <button className="btn">Google Login Recap</button>
+            <button onClick={handleGoogleLoginRecap} className="btn">Google Login Recap</button>
         </div>
     );
 };
